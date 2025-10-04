8895cce796645893f73e1f261f3d56e86a283160
Richard Garcia <code.richardy@gmail.com>
2025-10-04 19:22:27 -0300

    this commit introduces sort filters as a sealed record

--- Source/Filters/SortFilters.cs
+++ Source/Filters/SortFilters.cs
@@ -1,16 +1,13 @@
 namespace Vinder.Internal.Essentials.Filters;
 
-public sealed class SortFilters
+public sealed record SortFilters
 {
-    public string Field { get; set; }
+    public string Field { get; set; } = default!;
     public SortDirection Direction { get; set; }
 
-    private SortFilters(string field, SortDirection direction)
+    public static SortFilters From(string field, SortDirection direction = SortDirection.Ascending) => new()
     {
-        Field = field;
-        Direction = direction;
-    }
-
-    public static SortFilters From(string field, SortDirection direction = SortDirection.Ascending)
-        => new(field, direction);
+        Field = field,
+        Direction = direction
+    };
 }
