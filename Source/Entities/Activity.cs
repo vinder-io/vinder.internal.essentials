c4b04c665672dc6eeecb581b3851b62913be312b
Richard Garcia <code.richardy@gmail.com>
2025-10-04 20:21:58 -0300

    this commit introduces resource property to activity entity

--- Source/Entities/Activity.cs
+++ Source/Entities/Activity.cs
@@ -5,6 +5,7 @@ public sealed class Activity : Entity
     public string Action { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
 
+    public Resource? Resource { get; set; } = default!;
     public Tenant? Tenant { get; set; } = default!;
     public User? User { get; set; } = default!;
     public Dictionary<string, string> Metadata { get; set; } = [];
