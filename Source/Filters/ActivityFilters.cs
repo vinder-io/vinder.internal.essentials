1911bcf1e9daa4b1f724ad1b8862bb598dfe5056
Richard Garcia <code.richardy@gmail.com>
2025-10-04 20:22:07 -0300

    this commit introduces resource id and withspecifications method to activityfilters

--- Source/Filters/ActivityFilters.cs
+++ Source/Filters/ActivityFilters.cs
@@ -5,4 +5,7 @@ public sealed class ActivityFilters : Filters
     public string? Action { get; set; }
     public string? UserId { get; set; }
     public string? TenantId { get; set; }
+    public string? ResourceId { get; set; }
+
+    public static ActivityFiltersBuilder WithSpecifications() => new();
 }
